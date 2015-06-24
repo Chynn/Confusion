4349cd3f5ccc1f1c328da89487af42c45674d122
vthuyvi <s0541762@htw-berlin.de>
2015-06-24 21:01:14 +0200

    game activity first circle drawn

--- app/src/main/java/com/example/luuviettrinhle/confusion/GameActivity.java
+++ app/src/main/java/com/example/luuviettrinhle/confusion/GameActivity.java
@@ -1,6 +1,10 @@
 package com.example.luuviettrinhle.confusion;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,10 +22,33 @@ public class GameActivity extends ActionBarActivity {
         }
     }
 
+    public class MyView extends View{
+
+        public MyView(Context context){
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas){
+            super.onDraw(canvas);
+            int x = getWidth();
+            int y = getHeight();
+            int radius;
+            radius = 100;
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            canvas.drawPaint(paint);
+            // Use Color.parseColor to define HTML colors
+            paint.setColor(Color.parseColor("#CD5C5C"));
+            canvas.drawCircle(x / 2, y / 2, radius, paint);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(new MyView(this));
     }
 
 
