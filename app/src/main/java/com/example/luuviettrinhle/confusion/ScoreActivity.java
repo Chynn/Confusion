bac7789917b507c7332f913de62680d6a6808675
vthuyvi <s0541762@htw-berlin.de>
2015-07-02 17:47:29 +0200

    Merge branch 'master' of C:\Users\Vi\Documents\Uni\Sem 06\GDMA\Confusion with conflicts.

--- app/src/main/java/com/example/luuviettrinhle/confusion/ScoreActivity.java
+++ app/src/main/java/com/example/luuviettrinhle/confusion/ScoreActivity.java
@@ -8,43 +8,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-import dbSQLite.DBHandler;
 import dbSQLite.HighscoreActivity;
 
 
 public class ScoreActivity extends ActionBarActivity {
 
-    DBHandler db;
-    TextView scoreTV;
     EditText inputName;
+    String highscore;
 
     public void onButtonClick(View v) {
-        if (v.getId() == R.id.againButton) {
+        if (v.getId() == R.id.highscoreButton) {
+            Intent highscoreScreen = new Intent(this, HighscoreActivity.class);
+            highscoreScreen.putExtra("scoreTextView", highscore);
+            startActivity(highscoreScreen);
+        } else if (v.getId() == R.id.againButton) {
             Intent intent = new Intent(this, GameActivity.class);
             startActivity(intent);
-            finish();
         } else if (v.getId() == R.id.homeButton) {
             Intent intent = new Intent(this, StartActivity.class);
             startActivity(intent);
             finish();
         } else if (v.getId() == R.id.saveButton){
-            Intent intent = new Intent(this, HighscoreActivity.class);
-            startActivity(intent);
-            finish();
-        } else if (v.getId() == R.id.exitButton){
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            finish();
-            System.exit(0);
+            //put into db
         }
-        //else if (v.getId() == R.id.nameInput){
-        // inputName.setText("");
-
     }
 
     @Override
@@ -53,27 +41,42 @@ public class ScoreActivity extends ActionBarActivity {
         setContentView(R.layout.activity_score);
 
         Typeface bauhausFont = Typeface.createFromAsset(getAssets(), "bauhaus.ttf");
-        Typeface bauFont = Typeface.createFromAsset(getAssets(), "bauhaus.ttf");
-        Typeface arialFont = Typeface.createFromAsset(getAssets(), "arial.ttf");
-
         TextView sTitle = (TextView)findViewById(R.id.scoreTitle);
-        TextView score = (TextView)findViewById(R.id.scoreTextView);
-        TextView sInput = (TextView)findViewById(R.id.nameInput);
-
         sTitle.setTypeface(bauhausFont);
-        score.setTypeface(bauFont);
+
+        Typeface arialFont = Typeface.createFromAsset(getAssets(), "arial.ttf");
+        TextView sInput = (TextView)findViewById(R.id.nameInput);
         sInput.setTypeface(arialFont);
 
-        scoreTV = (TextView)findViewById(R.id.scoreTextView);
+        TextView scoreTV= (TextView)findViewById(R.id.scoreTextView);
         inputName = (EditText)findViewById(R.id.nameInput);
 
         Intent intent = getIntent();
-        String highscore = intent.getStringExtra("scoreTextView");
-
-        Intent i = getIntent();
-        String name = i.getStringExtra("nameInput");
+        highscore = intent.getStringExtra("scoreTextView");
 
         scoreTV.setText(highscore);
-        inputName.setText(name);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_score, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
